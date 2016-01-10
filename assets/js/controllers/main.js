ea0e7f320200dfe94d9b669aebf6236435585a25
mkhairul <mkhairul@gmail.com>
2016-01-10 21:17:45 +0800

    updates

--- assets/js/controllers/main.js
+++ assets/js/controllers/main.js
@@ -1,6 +1,14 @@
 app.controller('MainController',
-  ['$scope', '$animate', 'localStorageService', 'todoService', '$alert', '$timeout', '$rootScope',
-  function($scope, $animate, localStorageService, todoService, $alert, $timeout, $rootScope){
+  ['$scope', '$animate', 'localStorageService', 'todoService', '$alert', '$timeout', '$rootScope', '$http',
+  function($scope, $animate, localStorageService, todoService, $alert, $timeout, $rootScope, $http){
 
-  
+  $http.get('http://admin.skynatures.com/products')
+	.success(function(data){
+		console.log(data);
+		$scope.products = data;
+	})
+	.error(function(){
+	});
+	
+		
 }]);
